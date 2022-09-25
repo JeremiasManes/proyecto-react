9dbbb8ac8a1a56de218bbd86f3b700942bc43580
Jeremias Manes <jereemanes@gmail.com>
2022-09-25 18:41:39 -0300

    Se eliminaron archivos innecesarios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola
-        </p>
-      </header>
+      <Navbar />
     </div>
   );
 }
