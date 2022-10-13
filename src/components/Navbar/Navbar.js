cdb767e996c1107b0d90943a8d24e68d4f3d0934
Jeremias Manes <jereemanes@gmail.com>
2022-10-13 16:22:39 -0300

    css añadido al navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import './Navbar.css'
 import CartWidget from '../CartWidget/CartWidget'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 
 const Navbar = () => {
     return (
         <nav className='nav'>
-            <Link to={'/'}><h1 className='marca'>ICHIRO</h1></Link>
-            <div className='categorias'>
-                <Link to={'/category/buzos'}>BUZOS</Link>
-                <Link to={'/category/remeras'}>REMERAS</Link>
-                <Link to={'/category/zapatillas'}>ZAPATILLAS</Link>
+            <Link to={'/'} className='marca'>ICHIRO</Link>
+            <div className='divcategorias'>
+                <NavLink to={'/category/buzos'} className={({isActive}) => (isActive ? 'categoriasActive' : 'categorias')}>BUZOS</NavLink>
+                <NavLink to={'/category/remeras'} className={({isActive}) => (isActive ? 'categoriasActive' : 'categorias')}>REMERAS</NavLink>
+                <NavLink to={'/category/zapatillas'} className={({isActive}) => (isActive ? 'categoriasActive' : 'categorias')}>ZAPATILLAS</NavLink>
             </div>
             <CartWidget />
         </nav>
